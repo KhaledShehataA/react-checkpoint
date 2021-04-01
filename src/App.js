ce55010b9db380052c9c28325847d05a77b3c45c
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-04-01 20:34:21 +0200

    simple form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <section className="get_in_touch">
+      <h1 className="title">get in touch</h1>
+
+      <div className="container">
+        <div className="contact-form row">
+
+          <div className="form-field col-lg-6">
+            <input id="name" className="input-text" type="text"/>
+            <label htmlFor="name" className="label">name</label>
+          </div>
+
+          <div className="form-field col-lg-6">
+            <input id="email" className="input-text" type="email"/>
+            <label htmlFor="email" className="label">email</label>
+          </div>
+
+          <div className="form-field col-lg-6">
+            <input id="phone" className="input-text" type="text"/>
+            <label htmlFor="phone" className="label">contact number</label>
+          </div>
+
+          <div className="form-field col-lg-6">
+            <input id="message" className="input-text" type="text"/>
+            <label htmlFor="message" className="label">message</label>
+          </div>
+
+          <div className="form-field col-lg-6">
+            <input className="submit-btn" type="submit" value="submit" name="" id=""/>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
   );
 }
 
