2640f4d3ec01923d610e8e771b9920ed03fdceed
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-04-13 17:12:51 +0200

    refactor: remove unused code

--- src/App.js
+++ src/App.js
@@ -5,33 +5,33 @@ function App() {
   return (
    
     <section className="get_in_touch">
-      <h1 className="title">get in touch</h1>
+      <h1 className="title text-uppercase text-center">get in touch</h1>
 
       <div className="container">
         <div className="contact-form row">
 
           <div className="form-field col-lg-6">
-            <input id="name" className="input-text" type="text"/>
+            <input className="input-text" type="text"/>
             <label htmlFor="name" className="label">name</label>
           </div>
 
           <div className="form-field col-lg-6">
-            <input id="email" className="input-text" type="email"/>
+            <input className="input-text" type="email"/>
             <label htmlFor="email" className="label">email</label>
           </div>
 
           <div className="form-field col-lg-6">
-            <input id="phone" className="input-text" type="text"/>
+            <input className="input-text" type="text"/>
             <label htmlFor="phone" className="label">contact number</label>
           </div>
 
           <div className="form-field col-lg-6">
-            <input id="message" className="input-text" type="text"/>
+            <input className="input-text" type="text"/>
             <label htmlFor="message" className="label">message</label>
           </div>
 
           <div className="form-field col-lg-6">
-            <input className="submit-btn" type="submit" value="submit" name="" id=""/>
+            <input className="submit-btn" type="submit" value="submit" />
           </div>
 
         </div>
